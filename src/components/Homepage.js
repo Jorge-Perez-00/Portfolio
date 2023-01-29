c986e83a4368dec84404f01b705e007a8b47a568
Jorge Perez <jorge.perez59@myhunter.cuny.edu>
2023-01-28 23:13:01 -0500

    I set up react router

---
+++ src/components/Homepage.js
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+
+
+class Homepage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+
+    }
+
+
+    render() {
+        return (
+            <div className="main-container">
+                <h1 style={{color: "white"}}>Homepage</h1>
+            </div>
+
+
+        )
+    }
+}
+
+export default Homepage;
