0f17b86c1523a4f380b9b1b0404eff617d336de9
Jorge Perez <jorge.perez59@myhunter.cuny.edu>
2023-04-12 18:57:31 -0400

    Implemented the skills game page and added content

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 
 function Layout(props) {
 
-    const {game} = props;
+    const {game, topFixedLayer} = props;
     const navigate = useNavigate();
     const headerRef = useRef(null);
 
@@ -43,9 +43,19 @@ function Layout(props) {
             {game && 
                 <div className='main-art-container'>
                     <img src={require(`../images/title-${game}.png`)} alt="main-title" className='main-page-title' />
-                    <div className={`main-art ${game}-art`}>
+                    <div className={`main-art-layers layer-1 ${game}-art`}>
 
                     </div>
+
+                    <div className={`main-art-layers layer-2 ${game}-art-layer-2`}>
+
+                    </div>
+
+                    {topFixedLayer && 
+                        <div className={`top-layer-art-skills`}>
+
+                        </div>
+                    }
 
                     <div className='page-arrow-down'>
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,93 @@ function Skills(props) {
 
 
     return (
-        <Layout game={"skills"}>
+        <Layout game={"skills"} topFixedLayer={true}>
+
+            <div className="image-cards-container">
+                <div className="image-card">
+                    <div className="image-card-face">
+                        <div className="image-card-front">
+                            <img src={require(`../images/javascript-art.png`)} alt="test" className="image-card-image pixel-art-images" />                  
+
+                        </div>
+                        <div className="image-card-back">
+                            <h3 className="image-card-title" >Javascript</h3>
+                            <p className="image-card-text">Javascript is the first language that introduced me to programming in high school. I started to seriously learn Javascript in my final year of college for frontend development.</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="image-card">
+                    <div className="image-card-face">
+                        <div className="image-card-front">
+                            <img src={require(`../images/html-art.png`)} alt="test" className="image-card-image pixel-art-images" />
+                        </div>
+                        <div className="image-card-back">
+                            <h3 className="image-card-title" >HTML</h3>
+                            <p className="image-card-text">I began learning HTML in my intro to web development class. I gained an interest in frontend development from learning HTML.</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="image-card">
+                    <div className="image-card-face">
+                        <div className="image-card-front">
+                            <img src={require(`../images/css-art.png`)} alt="test" className="image-card-image pixel-art-images" />
+                        </div>
+                        <div className="image-card-back">
+                            <h3 className="image-card-title" >CSS</h3>
+                            <p className="image-card-text">I started to learn CSS when I started to get comfortable with building basic websites and to this day I continue to challenge myself and learn even more about CSS styling and techniques.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="image-card">
+                    <div className="image-card-face">
+                        <div className="image-card-front">
+                            <img src={require(`../images/reactjs-art.png`)} alt="test" className="image-card-image pixel-art-images" />
+                        </div>
+                        <div className="image-card-back">
+                            <h3 className="image-card-title" >React JS</h3>
+                            <p className="image-card-text">React JS is currently my main frontend development tool. I started using React right after I got comfortable with HTML and Javascript. All of my projects showcased in my portfolio were all developed using React.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="image-card">
+                    <div className="image-card-face">
+                        <div className="image-card-front">
+                            <img src={require(`../images/c++-art.png`)} alt="test" className="image-card-image pixel-art-images" />
+                        </div>
+                        <div className="image-card-back">
+                            <h3 className="image-card-title" >C++</h3>
+                            <p className="image-card-text">C++ is the programming language that introduced me to all the basic fundamentals of programming like classes and functions. C++ was the programming language used in 90% of computer science classes at Hunter College. </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="image-card">
+                    <div className="image-card-face">
+                        <div className="image-card-front">
+                            <img src={require(`../images/python-art.png`)} alt="test" className="image-card-image pixel-art-images" />
+                        </div>
+                        <div className="image-card-back">
+                            <h3 className="image-card-title" >Python</h3>
+                            <p className="image-card-text">Python was the first programming language I used in college and the language that further helped grow my interest in computer science.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="image-card">
+                    <div className="image-card-face">
+                        <div className="image-card-front">
+                            <img src={require(`../images/firebase-art.png`)} alt="test" className="image-card-image pixel-art-images" />
+                        </div>
+                        <div className="image-card-back">
+                            <h3 className="image-card-title" >Firebase</h3>
+                            <p className="image-card-text">I used Firebase to implement a simple 4 player online multiplayer maze game mode in my maze reinforcement learning project.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </Layout>
 
     )
